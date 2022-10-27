7db81807049a894a75e26661f388f445f1352087
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 20:58:44 +0300

    Update 02page2.py

--- pages/02page2.py
+++ pages/02page2.py
@@ -12,8 +12,10 @@ option = st.selectbox('Select the Compmay name',df['name'].unique())
 df1=df.loc[df['name'] == option]
 #df1 = df1.rename(columns={'date':'index'}).set_index('index')
 #chart_data = df1[['open','high','low','close','volume_traded ']].copy()
-fig = plt.figure() 
-plt.plot(df1['close']) 
+#fig = plt.figure() 
+#plt.plot(df1['close']) 
+plt.figure(); df1.plot(); plt.legend(loc='best')
+
 st.pyplot(plt)
              
 
