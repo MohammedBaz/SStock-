0ca0567ab535e71af8b407266fedb89bb2a6bdd0
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 19:56:10 +0300

    Update 02page2.py

--- pages/02page2.py
+++ pages/02page2.py
@@ -6,4 +6,6 @@ df = pd.read_csv('out.csv')
 option = st.selectbox('Select the Compmay name',df['name'].unique())
 df1=df.loc[df['name'] == option]
 df1 = df1.rename(columns={'date':'index'}).set_index('index')
-st.line_chart(df1['close', 'open'])
+
+chart_data = df1[['open','high','low','close','volume_traded','no_trades']].copy()
+st.line_chart(chart_data)
