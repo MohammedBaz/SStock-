07add465b12d70fc3b7fc95abeb6a5c23d62154c
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 19:08:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 
-st.markdown("# Main page 🎈")
+st.markdown("#This project was created for research purposes. It is not intended to be used as recommendations system and we disclaim any responsibility for any misuse of it 🎈")
 st.sidebar.markdown("# Main page 🎈")
 df = pd.read_csv('out.csv')
 st.dataframe(df)
