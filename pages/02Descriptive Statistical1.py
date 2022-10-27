c0e058eda2d6a2545ef1c7a807d6b74ace26b37e
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 22:17:22 +0300

    Update 02Descriptive Statistical1.py

--- pages/02Descriptive Statistical1.py
+++ pages/02Descriptive Statistical1.py
@@ -16,18 +16,3 @@ plt.figure()
 chart_data.plot(subplots=True, figsize=(6, 6))
 plt.legend(loc='best');plt.xticks(rotation=90) # for more plot option see https://pandas.pydata.org/pandas-docs/version/0.13/visualization.html
 st.pyplot(plt)
-
-dchart_data = pd.DataFrame(chart_data.describe(include='all'))
-st.dataframe(dchart_data)  # Same as st.write(df)
-st.write('One day Autocorrelation',chart_data['open'].autocorr(lag=1))
-st.write('One week Autocorrelation',chart_data['open'].autocorr(lag=7))
-st.write('One month Autocorrelation',chart_data['open'].autocorr(lag=30))
-st.write('One year Autocorrelation',chart_data['open'].autocorr(lag=365))
-
-from statsmodels.tsa.stattools import adfuller
-adft = adfuller(df1['open'],autolag="AIC")
-output_df = pd.DataFrame({"Values":[adft[0],adft[1],adft[2],adft[3], adft[4]['1%'], adft[4]['5%'], adft[4]['10%']]  , "Metric":["Test Statistics","p-value","No. of lags used","Number of observations used", 
-                                                        "critical value (1%)", "critical value (5%)", "critical value (10%)"]})
-st.write(output_df)
-
-
