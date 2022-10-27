bf5bbbe7e7563df8979d1bed553c9d3c2990306d
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 18:56:51 +0300

    Update and rename page3.py to 03page3.py

--- pages/03page3.py
+++ pages/03page3.py
@@ -1,4 +1,5 @@
 # Contents of ~/my_app/pages/page_3.py
+#Again the (03) of the 03page3.py has been added to ensure the correct sort of pages only 
 import streamlit as st
 
 st.markdown("# Page 3 🎉")
