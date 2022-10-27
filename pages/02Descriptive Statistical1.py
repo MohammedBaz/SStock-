f35f02c56b4bf8653dc309567eee3c4c637f9bbf
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 23:38:42 +0300

    Update 02Descriptive Statistical1.py

--- pages/02Descriptive Statistical1.py
+++ pages/02Descriptive Statistical1.py
@@ -5,8 +5,9 @@ import pandas as pd
 import altair as alt
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import date
 
-df = pd.read_csv('out.csv',parse_dates=date1)
+df = pd.read_csv('out.csv',parse_dates='date1')
 
 option = st.selectbox('Select the Compmay name',df['name'].unique())
 df1=df.loc[df['name'] == option]
