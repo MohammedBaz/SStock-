182e84601cb1ba43994ad689f368dfb4ef0f950c
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 20:38:12 +0300

    Update 02page2.py

--- pages/02page2.py
+++ pages/02page2.py
@@ -4,13 +4,18 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
+import matplotlib.pyplot as plt
+
 df = pd.read_csv('out.csv')
 
 option = st.selectbox('Select the Compmay name',df['name'].unique())
 df1=df.loc[df['name'] == option]
 df1 = df1.rename(columns={'date':'index'}).set_index('index')
 chart_data = df1[['open','high','low','close']].copy()
-st.bar_chart(chart_data)
+fig = plt.figure() 
+plt.plot(chart_data) 
+
+st.pyplot(fig)
 
 
 #chart_data = df1[['open','high','low','close','no_trades ']].copy()
