88874ae62fe97591e7afd94a3558d38b2384e93f
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 19:19:45 +0300

    Update 02page2.py

--- pages/02page2.py
+++ pages/02page2.py
@@ -1,6 +1,8 @@
 # Contents of ~/my_app/pages/page_2.py
 # the (02) prefix of 02page2.py has been added to ensure the correct sort of pages only 
 import streamlit as st
-option = st.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
+
+
+
+option = st.selectbox('Select the Compmay name',
+                      (df['name'].unique())
