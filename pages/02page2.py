43fbf34e0b7554ab14f7d6ee35a83f18cd480de6
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 18:56:09 +0300

    Update and rename page2.py to 02page2.py

--- pages/02page2.py
+++ pages/02page2.py
@@ -1,4 +1,5 @@
 # Contents of ~/my_app/pages/page_2.py
+# the (02) prefix of 02page2.py has been added to ensure the correct sort of pages only 
 import streamlit as st
 
 st.markdown("# Page 2 ❄️")
