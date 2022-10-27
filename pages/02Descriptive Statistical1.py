eb8df1a94f3c427e51c42b0843599e78b221d388
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 22:43:30 +0300

    Update 02Descriptive Statistical1.py

--- pages/02Descriptive Statistical1.py
+++ pages/02Descriptive Statistical1.py
@@ -11,7 +11,7 @@ df = pd.read_csv('out.csv')
 option = st.selectbox('Select the Compmay name',df['name'].unique())
 df1=df.loc[df['name'] == option]
 df1 = df1.rename(columns={'date':'index'}).set_index('index')
-chart_data = df1[['open','high','low','close','volume_traded ','no_trades ']].copy()
+chart_data = df1[['open','high','low','close','volume_traded ','no_trades ','value_traded']].copy()
 plt.figure()
 chart_data.plot(subplots=True, figsize=(6, 6))
 plt.legend(loc='best');plt.xticks(rotation=90) # for more plot option see https://pandas.pydata.org/pandas-docs/version/0.13/visualization.html
