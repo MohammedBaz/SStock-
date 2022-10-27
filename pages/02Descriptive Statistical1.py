1cd5a2f28ae30ed578c1207c5248a402777e61e6
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-28 01:20:02 +0300

    Update 02Descriptive Statistical1.py

--- pages/02Descriptive Statistical1.py
+++ pages/02Descriptive Statistical1.py
@@ -16,14 +16,14 @@ df1=df.loc[df['name'] == option]
 fig, ax = plt.subplots()
 ax= sns.lineplot(data=df1, x="date1", y="open")
 ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
-ax.set_xlabel('Opening price')  
-ax.yaxis.set_label_text('Date')
+ax.set_xlabel('Date')  
+ax.yaxis.set_label_text('Opening price')
 st.pyplot(plt)
 
 fig, ax = plt.subplots()
 ax1= sns.lineplot(data=df1, x="date1", y="close")
 ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
-ax.set_xlabel('Closing price')  
+ax.set_xlabel('Date')  
 ax.yaxis.set_label_text('Closing price')
 st.pyplot(plt)
 
@@ -31,7 +31,7 @@ fig, ax = plt.subplots()
 ax2= sns.lineplot(data=df1, x="date1", y="high")
 ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
 ax.set_xlabel('Date')  
-ax.yaxis.set_label_text('Date price')
+ax.yaxis.set_label_text('Highest price')
 st.pyplot(plt)
 
 fig, ax = plt.subplots()
@@ -62,34 +62,3 @@ ax.set_xlabel('Date')
 ax.yaxis.set_label_text('Trading price')
 st.pyplot(plt)
 
-#ax1 = sns.lineplot(x='x', y='z', data=data)
-
-
-
-#ax=sns.lineplot(data=df1, x="date1", y="open")
-#ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
-#plt.ylabel("Opening Price")
-#st.pyplot(plt)
-
-
-
-#fig, axes = plt.subplots(nrows=2, ncols=2)
-#plt.plot(df.date1, df.open, linewidth=3,)
-#sns.lineplot(df)
-#df1['date1','open'].plot(ax=axes[0,0]); axes[0,0].set_title('Opening Price')
-#df1['open'].plot(ax=axes[0,0]); axes[0,0].set_title('Opening Price')
-#df1['close'].plot(ax=axes[0,1]); axes[0,1].set_title('Closing Price')
-#df1['high'].plot(ax=axes[1,0]); axes[1,0].set_title('high Price')
-#df1['low'].plot(ax=axes[1,1]); axes[1,1].set_title('low Price')
-#st.pyplot(plt)
-#df1 = df1.rename(columns={'date1':'index'}).set_index('index')
-
-#plt.plot(df.x, df.y)
-
-#df1.set_index('date1')
-#chart_data = df1[['open','high','low','close','volume_traded ','no_trades ','value_traded','date1']].copy()
-#chart_data.set_index('date1')
-#plt.figure()
-#chart_data.plot(subplots=True, figsize=(6, 6))
-#plt.legend(loc='best');plt.xticks(rotation=90) # for more plot option see https://pandas.pydata.org/pandas-docs/version/0.13/visualization.html
-#st.pyplot(plt)
