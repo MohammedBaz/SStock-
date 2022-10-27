83f5a364c3de5dcfbf1e8cfe418d2cd30687d687
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 19:20:56 +0300

    Update 02page2.py

--- pages/02page2.py
+++ pages/02page2.py
@@ -1,7 +1,5 @@
 # Contents of ~/my_app/pages/page_2.py
 # the (02) prefix of 02page2.py has been added to ensure the correct sort of pages only 
 import streamlit as st
-
-
-
+df = pd.read_csv('out.csv')
 option = st.selectbox('Select the Compmay name',df['name'].unique())
