ada04727c10766c92cc2f1c675110b8e9a7bd2ae
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-28 01:31:58 +0300

    Update 02Descriptive Statistical1.py

--- pages/02Descriptive Statistical1.py
+++ pages/02Descriptive Statistical1.py
@@ -15,44 +15,44 @@ option = st.selectbox('Select the Compmay name',df['name'].unique())
 df1=df.loc[df['name'] == option]
 
 
-fig, (ax1, ax2,ax3, ax4,ax5, ax6,ax7) = plt.subplots( )
+fig, (ax1, ax2,ax3, ax4,ax5, ax6,ax7) = plt.subplots(2,4 )
 
 
 ax1= sns.lineplot(data=df1, x="date1", y="open")
 ax1.set_xticklabels(ax1.get_xticklabels(), rotation=40, ha="right")
 ax1.set_xlabel('Date')  
 ax1.yaxis.set_label_text('Opening price')
-st.pyplot(fig)
+
 
 ax2= sns.lineplot(data=df1, x="date1", y="close")
 ax2.set_xticklabels(ax2.get_xticklabels(), rotation=40, ha="right")
 ax2.set_xlabel('Date')  
 ax2.yaxis.set_label_text('Closing price')
-st.pyplot(fig)
+
 
 ax3= sns.lineplot(data=df1, x="date1", y="high")
 ax3.set_xticklabels(ax3.get_xticklabels(), rotation=40, ha="right")
 ax3.set_xlabel('Date')  
 ax3.yaxis.set_label_text('Highest price')
-st.pyplot(fig)
+
 
 ax4= sns.lineplot(data=df1, x="date1", y="low")
 ax4.set_xticklabels(ax4.get_xticklabels(), rotation=40, ha="right")
 ax4.set_xlabel('Date')    
 ax4.yaxis.set_label_text('Lowest price')
-st.pyplot(fig)
+
 
 ax5= sns.lineplot(data=df1, x="date1", y="volume_traded ")
 ax5.set_xticklabels(ax5.get_xticklabels(), rotation=40, ha="right")
 ax5.set_xlabel('Date')   
 ax5.yaxis.set_label_text('Traded Volume')
-st.pyplot(fig)
+
 
 ax6= sns.lineplot(data=df1, x="date1", y="no_trades ")
 ax6.set_xticklabels(ax6.get_xticklabels(), rotation=40, ha="right")
 ax6.set_xlabel('Date')   
 ax6.yaxis.set_label_text('Number of Trades')
-st.pyplot(fig)
+
 
 ax7= sns.lineplot(data=df1, x="date1", y="value_traded")
 ax7.set_xticklabels(ax7.get_xticklabels(), rotation=40, ha="right")
