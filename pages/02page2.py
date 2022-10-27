62b1fd730a3dfbb035eaeb164df3b4413d8cb106
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 20:04:24 +0300

    Update 02page2.py

--- pages/02page2.py
+++ pages/02page2.py
@@ -6,6 +6,6 @@ df = pd.read_csv('out.csv')
 option = st.selectbox('Select the Compmay name',df['name'].unique())
 df1=df.loc[df['name'] == option]
 df1 = df1.rename(columns={'date':'index'}).set_index('index')
-
 chart_data = df1[['open','high','low','close','no_trades ']].copy()
-st.line_chart(chart_data)
+import altair as alt
+alt.Chart(chart_data)
