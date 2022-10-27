6c38f60ba5ae01cc225c36bf8a76f11415d61ebe
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 20:23:46 +0300

    Update 02page2.py

--- pages/02page2.py
+++ pages/02page2.py
@@ -2,11 +2,27 @@
 # the (02) prefix of 02page2.py has been added to ensure the correct sort of pages only 
 import streamlit as st
 import pandas as pd
+import altair as alt
 df = pd.read_csv('out.csv')
+
 option = st.selectbox('Select the Compmay name',df['name'].unique())
 df1=df.loc[df['name'] == option]
 df1 = df1.rename(columns={'date':'index'}).set_index('index')
-chart_data = df1[['open','high','low','close','no_trades ']].copy()
-import altair as alt
-c = alt.Chart(df1['close'])
-st.altair_chart(c)
+chart1 = alt.Chart(df1).mark_line().encode(
+    x=alt.X('date',axis=alt.Axis(format='%Y-%m',labelAngle=-20)),
+    y=alt.Y('close',scale=alt.Scale(domain=[np.min(df1.colname1), np.max(df1.colname1)])), 
+).properties(width=500, height=300)
+chart2 = alt.Chart(df1).mark_line().encode(
+    x=alt.X('date',axis=alt.Axis(format='%Y-%m',labelAngle=-20)),
+    y=alt.Y('open',scale=alt.Scale(domain=[np.min(df1.colname2), np.max(df1.colname2)])), 
+).properties(width=500, height=300)
+st.altair_chart(chart1 | chart2)
+
+#chart_data = df1[['open','high','low','close','no_trades ']].copy()
+
+#c = alt.Chart(df1['close'])
+#st.altair_chart(c)
+
+
+
+
