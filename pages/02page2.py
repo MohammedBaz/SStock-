9a8e26711ea5838fb5c89889166aeaaee4bfad87
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-27 19:15:56 +0300

    Update 02page2.py

--- pages/02page2.py
+++ pages/02page2.py
@@ -1,6 +1,6 @@
 # Contents of ~/my_app/pages/page_2.py
 # the (02) prefix of 02page2.py has been added to ensure the correct sort of pages only 
 import streamlit as st
-
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
