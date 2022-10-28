65e308ab3082f61beb86dbbc443a7895c3667c16
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-28 04:12:20 +0300

    Update 05Autocorrleation.py

--- pages/05Autocorrleation.py
+++ pages/05Autocorrleation.py
@@ -15,17 +15,10 @@ df1=df.loc[df['name'] == option]
 X1=['open',	'high',	'low',	'close','volume_traded ','value_traded','no_trades ']
 st.dataframe(df1[X1].corr())
 
-import matplotlib.pyplot as plt
-import numpy as np
+
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 fig, ax = plt.subplots()
-colormap = plt.cm.RdBu
-plt.figure(figsize=(15,10))
-plt.title(u'6 hours', y=1.05, size=16)
-
-mask = np.zeros_like(df1[X1].corr())
-mask[np.triu_indices_from(mask)] = True
-
-svm = sns.heatmap(df1[X1].corr(), mask=mask, linewidths=0.1,vmax=1.0, 
-            square=True, cmap=colormap, linecolor='white', annot=True)
-st.pyplot(svm)
+sns.heatmap(df1[X1].corr().corr(), ax=ax)
+st.write(fig)
