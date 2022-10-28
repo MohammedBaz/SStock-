0d76211b9f17b2af9b03480e6e05f57c5a012bb7
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-28 12:46:33 +0300

    Create 07Decomposition.py

---
+++ pages/07Decomposition.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import date
+import seaborn as sns
+
+df = pd.read_csv('out.csv')
+df["date1"] = pd.to_datetime(df["date1"]) 
+df =df.sort_values(by='date1')
+
+option = st.selectbox('Select the Compmay name',df['name'].unique())
+df1=df.loc[df['name'] == option]
+
+X1=['open',	'high',	'low',	'close','volume_traded ','value_traded','no_trades ']
+
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+decompose = seasonal_decompose(df1['open'],model='additive', period=7)
+decompose.plot()
+st.write(plt)
