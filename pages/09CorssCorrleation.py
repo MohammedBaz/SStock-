513286c2fe19dc28d7fda4c0143685c9635cc63e
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-29 09:19:49 +0300

    Update 09CorssCorrleation.py

--- pages/09CorssCorrleation.py
+++ pages/09CorssCorrleation.py
@@ -8,9 +8,28 @@ import seaborn as sns
 df = pd.read_csv('out.csv')
 df["date1"] = pd.to_datetime(df["date1"]) 
 df =df.sort_values(by='date1')
+df.index = df['date1']
 
-options = st.multiselect('Select the Compmay name',df['name'].unique())
-st.write(len(options))
 
-#for Comapny in options:
-#  df
+SelectedName = st.multiselect('Select the Compmay name',df['name'].unique())
+
+def GenerateCrossCorrMat():
+  SelectedList=[]
+  ans=np.empty((len(SelectedName),len(SelectedName)), dtype=object)
+  #ans[0,1:]=SelectedName
+  #ans[1:,0]=SelectedName
+  for i in range(len(SelectedName)):
+    SelectedList.append([])
+    SelectedList[i]=df['open'].loc[df['name'] == SelectedName[i]]
+
+  for i in range (len(SelectedName)):
+    for j in range (len(SelectedName)):
+      print(i,j,SelectedList[i].corr(SelectedList[j]))
+      ans[i,j]=SelectedList[i].corr(SelectedList[j])
+  return (ans)
+
+ans=GenerateCrossCorrMat()
+dfans=pd.DataFrame(ans)
+dfans.columns = SelectedName
+dfans.index = SelectedName
+st.write(dfans)
