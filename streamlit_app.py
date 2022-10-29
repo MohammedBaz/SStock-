8235b7f63e4933a2ce7cce4cd351bde1c6e62b20
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-10-29 09:28:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,4 @@ import streamlit as st
 import pandas as pd
 
 st.markdown("This project was created for research purposes. It is not intended to be used as recommendations system and we disclaim any responsibility for any misuse of it")
-st.sidebar.markdown("# Main page")
-df = pd.read_csv('out.csv')
-st.dataframe(df)
+st.markdown("The dataset used to genra this app is taken from https://www.kaggle.com/code/salwaalzahrani/starter-saudi-stock-exchange-tadawul-b41b6455-6/data")
