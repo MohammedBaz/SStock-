9613116f0c68638630bcaa9338246356cf6356b1
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-11-01 00:31:04 +0300

    Update and rename 06StationarityAnalysis.py to 06AugmentedDickeyFullerAnalysis.py

--- pages/06AugmentedDickeyFullerAnalysis.py
+++ pages/06AugmentedDickeyFullerAnalysis.py
@@ -14,7 +14,41 @@ df1=df.loc[df['name'] == option]
 
 X1=['open',	'high',	'low',	'close','volume_traded ','value_traded','no_trades ']
 
+from statsmodels.tsa.stattools import adfuller 
+def adfuller_test(series, signif=0.05, name='', verbose=False):
+    """Perform ADFuller to test for Stationarity of given series and print report"""
+    r = adfuller(series, autolag='AIC')
+    output = {'test_statistic':round(r[0], 4), 'pvalue':round(r[1], 4), 'n_lags':round(r[2], 4), 'n_obs':r[3]}
+    p_value = output['pvalue'] 
+    def adjust(val, length= 6): return str(val).ljust(length)
 
+    # Print Summary
+    print(f'    Augmented Dickey-Fuller Test on "{name}"', "\n   ", '-'*47)
+    print(f' Null Hypothesis: Data has unit root. Non-Stationary.')
+    print(f' Significance Level    = {signif}')
+    print(f' Test Statistic        = {output["test_statistic"]}')
+    print(f' No. Lags Chosen       = {output["n_lags"]}')
+
+    for key,val in r[4].items():
+        print(f' Critical value {adjust(key)} = {round(val, 3)}')
+
+    if p_value <= signif:
+        print(f" => P-Value = {p_value}. Rejecting Null Hypothesis.")
+        print(f" => Series is Stationary.")
+    else:
+        print(f" => P-Value = {p_value}. Weak evidence to reject the Null Hypothesis.")
+        print(f" => Series is Non-Stationary.")  
+        
+        
+for name, column in df1.iteritems():
+    adfuller_test(column, name=column.name)
+    print('\n')
+
+
+
+
+
+"""
 from statsmodels.tsa.stattools import adfuller
 adft = adfuller(df1['open'],autolag="AIC")
 output_df = pd.DataFrame({"Values":[adft[0],adft[1],adft[2],adft[3], adft[4]['1%'], adft[4]['5%'], adft[4]['10%']]  , "Metric":["Test Statistics","p-value","No. of lags used","Number of observations used", 
@@ -35,3 +69,4 @@ adft = adfuller(df1['close'],autolag="AIC")
 output_df = pd.DataFrame({"Values":[adft[0],adft[1],adft[2],adft[3], adft[4]['1%'], adft[4]['5%'], adft[4]['10%']]  , "Metric":["Test Statistics","p-value","No. of lags used","Number of observations used", 
                                                         "critical value (1%)", "critical value (5%)", "critical value (10%)"]})
 st.write(output_df)
+"""
