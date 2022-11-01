187e3beef79220ff5150feb4e03b3d05997b714c
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-11-01 19:47:59 +0300

    Update 011TASIPrediction.py

--- pages/011TASIPrediction.py
+++ pages/011TASIPrediction.py
@@ -2,5 +2,5 @@ import streamlit as st
 import keras
 import numpy as np
 from tensorflow.keras.models import load_model
-model = load_model('/content/model.h5')
+model = load_model('modelx.h5')
 st.write(model.summary())
